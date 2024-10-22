8294a4c50f01fec21f65e126a132f48c8234e278
MuffinHeiler240 <jonathangreive@gmail.com>
2024-10-22 10:52:14 +0200

    Fixed CMake for Praktikum1

--- main.c
+++ main.c
@@ -4,15 +4,14 @@
 int main(void) {
     printf("hello world \n \n");
 
-    const int kList[3] = {1, 3, 6};
+    int kList[3] = {1, 3, 6};
     int p = 0;
     while (p < 3) {
-        const double a = pow(10,kList[p]) + 1./3.;
-        const double b = pow(10,kList[p]) + 1./7.;
+        double a = pow(10,kList[p]) + 1/3;
+        double b = pow(10,kList[p]) + 1/7;
         p++;
 
         double c=a+b;
-
         printf("a=%f + b=%f = c=%f\n", a, b, c);
 
         c=a-b;
@@ -26,7 +25,7 @@ int main(void) {
     }
 
 
-    constexpr int n = 10;
+    int n = 10;
     int Numbers[n][4];
 
     int i = 0;
@@ -47,7 +46,9 @@ int main(void) {
 
     printf("n   | n*n | s(n) | q(n)\n");
     printf("----+-----+------+------\n");
-    for (int k = 0; k < n; k++) {
+
+    int k;
+    for ( k = 0; k < n; k++) {
         printf("%3d | %4d| %4d | %4d\n", Numbers[k][0], Numbers[k][1], Numbers[k][2], Numbers[k][3]);
     }
     return 0;
