ebe0b3685292ea13e40e231e31a0d6c0a34844ed
MuffinHeiler240 <jonathangreive@gmail.com>
2024-10-29 14:33:23 +0100

    Made Praktikum1 Task 3 more easy to read

--- main.c
+++ main.c
@@ -2,16 +2,24 @@
 #include <math.h>
 
 int main(void) {
+    int kList[3] = {1,3,7};
+    int p = 0;
+    int steps = 10;
+    int start = 1;
+    int k;
+    int kSqu;
+    int kSum = 0;
+    int kSquSum = 0;
+
     printf("hello world \n \n");
 
-    int kList[3] = {1, 3, 6};
-    int p = 0;
     while (p < 3) {
         double a = pow(10,kList[p]) + 1/3;
         double b = pow(10,kList[p]) + 1/7;
-        p++;
 
-        double c=a+b;
+        double c;
+
+        c=a+b;
         printf("a=%f + b=%f = c=%f\n", a, b, c);
 
         c=a-b;
@@ -22,17 +30,11 @@ int main(void) {
 
         c=a/b;
         printf("a=%f / b=%f = c=%f\n \n", a, b, c);
+
+        p++;
     }
     /* Ergebnisse werden von double zu float zu int ungenauer! */
     /* %f für float und double; %i für int */
-
-    int steps = 10;
-    int start = 1;
-
-    int k ;
-    int kSqu;
-    int kSum = 0;
-    int kSquSum = 0;
 
     printf("n   | n*n | s(n) | q(n)\n");
     printf("----+-----+------+------\n");
